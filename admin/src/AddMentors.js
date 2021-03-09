33a52901460ae1acba5c1f8f7cb5bba0577c79cb
alannahpeash <aepeash@gmail.com>
2021-03-08 20:59:45 -0500

    update addMentors

--- admin/src/AddMentors.js
+++ admin/src/AddMentors.js
@@ -2,185 +2,189 @@ import React from "react";
 import Firebase from "firebase";
 import config from "./config";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 
 
-
 export class AddMentors extends React.Component {
-  constructor(props) {
-    super(props);
-    // Firebase.initializeApp(config.firebase);
-    if (!Firebase.apps.length) {
-      Firebase.initializeApp(config);
-   }else {
-      Firebase.app(); // if already initialized, use that one
-   }
-    this.usersRef = Firebase.firestore().collection('admins');
-    this.state = {
-      developers: []
+    constructor(props) {
+        super(props);
+        // Firebase.initializeApp(config.firebase);
+        if (!Firebase.apps.length) {
+            Firebase.initializeApp(config);
+        }else {
+            Firebase.app(); // if already initialized, use that one
+        }
+        this.usersRef = Firebase.firestore().collection('mentors');
+        this.mentors = []
+        this.state = {
+            developers: []
+        };
+
+    }
+
+    componentDidMount() {
+        console.log("umm tf is this called?", this.mentors);
+
+        this.getUserData();
+        console.log("umm tf is this called?", this.mentors);
+    }
+
+    // componentDidUpdate(prevProps, prevState) {
+    //   if (prevState !== this.state) {
+    //     this.getUserData();
+    //   }
+    // }
+
+    writeUserData = async (displayName, email, password) => {
+        let newUser = {
+            displayName: displayName,
+            email: email,
+            password: password
+        }
+        let newUserDocRef = await this.usersRef.add(newUser);
+        // this.usersRef
+        //   .add(this.state);
+        let key = newUserDocRef.id;
+        newUser.key = key;
+        this.mentors.push(newUser);
+        this.setState({developers: newUser});
+        console.log(newUserDocRef);
     };
 
-  }
+    getUserData = async () => {
+        // let ref = this.usersRef.get();
+        let querySnap = await this.usersRef.get();
+        querySnap.forEach(qDocSnap => {
+            let key = qDocSnap.id;
+            let data = qDocSnap.data();
+            data.key = key;
+            this.mentors.push(data);
+            this.setState({developers: data});
+        })
+        console.log("getuser data", this.mentors);
 
-  componentDidMount() {
-    this.getUserData();
-  }
+        // ref.on("value", snapshot => {
+        //   const state = snapshot.val();
+        //   this.setState(state);
+        // });
+    };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState !== this.state) {
-      this.writeUserData();
-    }
-  }
+    handleSubmit = event => {
+        event.preventDefault();
+        let email = this.refs.email.value;
+        let password = this.refs.password.value;
+        let displayName = this.refs.displayName.value;
 
-  writeUserData = () => {
-    this.usersRef
-      .add(this.state);
-    console.log("DATA SAVED");
-  };
-
-  getUserData = async () => {
-    // let ref = this.usersRef.get();
-    let querySnap = await this.usersRef.get();
-    querySnap.forEach(qDocSnap => {
-      let key = qDocSnap.id;
-      let data = qDocSnap.data();
-      data.key = key;
-      this.setState(data);
-    // ref.on("value", snapshot => {
-    //   const state = snapshot.val();
-    //   this.setState(state);
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    let name = this.refs.name.value;
-    let role = this.refs.role.value;
-    let uid = this.refs.uid.value;
-
-    if (uid && name && role) {
-      const { developers } = this.state;
-      const devIndex = developers.findIndex(data => {
-        return data.uid === uid;
-      });
-      developers[devIndex].name = name;
-      developers[devIndex].role = role;
-      this.setState({ developers });
-    } else if (name && role) {
-      const uid = new Date().getTime().toString();
-      const { developers } = this.state;
-      developers.push({ uid, name, role });
-      this.setState({ developers });
+        this.writeUserData(displayName, email, password);
+        console.log("submitted", displayName, email, password);
     }
 
-    this.refs.name.value = "";
-    this.refs.role.value = "";
-    this.refs.uid.value = "";
-  };
+    removeData = developer => {
+        const { developers } = this.state;
+        const newState = developers.filter(data => {
+           return data.displayName !== developer.displayName;
+        });
+        this.setState({ developers: newState });
+     };
 
-  removeData = developer => {
-    const { developers } = this.state;
-    const newState = developers.filter(data => {
-      return data.uid !== developer.uid;
-    });
-    this.setState({ developers: newState });
-  };
+    updateData = developer => {
+        this.refs.displayName.value = developer.displayName;
+        this.refs.email.value = developer.email;
+        this.refs.password.value = developer.password;
+    };
 
-  updateData = developer => {
-    this.refs.uid.value = developer.uid;
-    this.refs.name.value = developer.name;
-    this.refs.role.value = developer.role;
-  };
+    render() {
+        const { developers } = this.state;
+        console.log("hello render", developers);
+        return (
 
-  render() {
-    const { developers } = this.state;
-    return (
-     
-      <React.Fragment>
-        
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-12">
-              <h1>Add a mentor</h1>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xl-12">
-              {developers.map(developer => (
-                <div
-                  key={developer.uid}
-                  className="card float-left"
-                  style={{ width: "18rem", marginRight: "1rem" }}
-                >
-                  <div className="card-body">
-                    <h5 className="card-title">{developer.name}</h5>
-                    <p className="card-text">{developer.role}</p>
-                    <button
-                      onClick={() => this.removeData(developer)}
-                      className="btn btn-link"
-                    >
-                      Delete
-                    </button>
-                    <button
-                      onClick={() => this.updateData(developer)}
-                      className="btn btn-link"
-                    >
-                      Edit
-                    </button>
-                  </div>
+            <React.Fragment>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-12">
+                            <h1>Create a mentor</h1>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xl-12">
+                            {this.mentors.map(developer => (
+                                <>
+                                    <div
+                                        //key not needed with react.fragment
+                                        key={developer.displayName}
+                                        className="card float-left"
+                                        style={{ width: "18rem", marginRight: "1rem" }}
+                                    >
+                                        <div className="card-body">
+                                            <h5 className="card-title" >Name: {developer.displayName}</h5>
+                                            <h5 className="card-title">Email: {developer.email}</h5>
+                                            {/*<p className="card-text">{developer.password}</p>*/}
+                                           {/* <button
+                                                onClick={() => this.removeData(developer)}
+                                                className="btn btn-link"
+                                            >
+                                                Delete
+                                            </button>
+                                            <button
+                                                onClick={() => this.updateData(developer)}
+                                                className="btn btn-link"
+                                            >
+                                                Edit
+                                            </button>*/}
+                                        </div>
+                                    </div>
+                                </>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xl-12">
+                            <h1>Add new mentor here</h1>
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-row">
+                                    <div className="form-group col-md-6">
+                                        <label>Name</label>
+                                        <input
+                                            type="text"
+                                            ref="displayName"
+                                            className="form-control"
+                                            placeholder="Name"
+                                        />
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        <label>Email</label>
+                                        <input
+                                            type="text"
+                                            ref="email"
+                                            className="form-control"
+                                            placeholder="Email"
+                                        />
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        <label>Password</label>
+                                        <input
+                                            type="text"
+                                            ref="password"
+                                            className="form-control"
+                                            placeholder="Password"
+                                        />
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-primary">
+                                    Save
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-              ))}
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xl-12">
-              <h1>Add new team member here</h1>
-              <form onSubmit={this.handleSubmit}>
-                <div className="form-row">
-                  <input type="hidden" ref="uid" />
-                  <div className="form-group col-md-6">
-                    <label>Name</label>
-                    <input
-                      type="text"
-                      ref="name"
-                      className="form-control"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div className="form-group col-md-6">
-                    <label>Role</label>
-                    <input
-                      type="text"
-                      ref="role"
-                      className="form-control"
-                      placeholder="Role"
-                    />
-                  </div>
-                </div>
-                <button type="submit" className="btn btn-primary">
-                  Save
-                </button>
-              </form>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xl-12">
-              <h3>
-                Tutorial{" "}
-                <a href="https://sebhastian.com/react-firebase-real-time-database-guide">
-                  here
-                </a>
-              </h3>
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+            </React.Fragment>
+        );
+    }
 }
 
 
